82977e1ce40722d69692e7edcf69e603bdc6649c
satyamdevs <satyam.2428it1663@kiet.edu>
2025-11-13 02:34:07 +0530

    header

--- finora_ai/app/page.js
+++ finora_ai/app/page.js
@@ -2,6 +2,8 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
+    <div className="mt-40">
     <Button variant="destructive">hdsfkgjsgbksghf</Button>
+    </div>
   );
 }
